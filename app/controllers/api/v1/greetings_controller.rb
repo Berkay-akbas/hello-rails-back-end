22110378a1cc5bc4e8579cd71d463614dbb0e2cf
Berkay Akbas <berkayakbas@gmail.com>
2022-11-29 16:36:09 +0300

    Create backend for greeting

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# rubocop:disable all
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        @greeting = Greeting.all.sample.text
+        render json: { message: @greeting }
+      end
+    end
+  end
+end
